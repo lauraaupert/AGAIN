b94b82bb9ec6927232b3b4b20ab2b311cb47b113
Laura Aupert <aupert.laura@gmail.com>
2021-02-02 12:44:40 -0500

    cleaned up apiRoutes.js code

--- apiRoutes.js
+++ apiRoutes.js
@@ -2,29 +2,22 @@ const fs = require("fs");
 var notes = require("./db/db.json")
 
 module.exports = (app) => {
-    app.get("/api/notes", function(req, res) {
+        app.get("/api/notes", function(req, res) {
         return res.json(notes)
-    })
+    });
 
     app.post("/api/notes", function(req, res) {
-    let newNote = req.body;
-    newNote.id = notes.length + 1;
-    
-    
-    notes.push(newNote)
-    console.log(newNote);
-    res.json(newNote);
-    })
+        let newNote = req.body;
+        newNote.id = notes.length + 1;
+        notes.push(newNote)
+        res.json(newNote);
+    });
 
     app.delete("/api/notes/:id", function(req, res) {
-        var delNote = req.params.id;
-        
-        console.log(delNote);
         notes.splice((req.params.id - 1), 1)
         for (var i = 0; i < notes.length; i++) {
-        notes[i].id = i + 1
+            notes[i].id = i + 1
         }
-        console.log(notes)
         res.json(notes)
-    })
+    });
 }
