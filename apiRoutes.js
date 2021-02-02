3445be523ea2dbcd1af5f33549479df4994651d6
Laura Aupert <aupert.laura@gmail.com>
2021-02-02 03:48:01 -0500

    With app.delete code that does delete but not always the right one.

--- apiRoutes.js
+++ apiRoutes.js
@@ -15,6 +15,29 @@ module.exports = (app) => {
     console.log(newNote);
     res.json(newNote);
     })
+
+    app.delete("/api/notes/:id", function(req, res) {
+        var delNote = req.params.id;
+        
+
+        console.log(delNote);
+        notes.splice((req.params.id - 1), 1)
+        for (var i = 0; i < notes.length; i++) {
+        notes.id = i + 1
+        }
+        console.log(notes)
+        res.json(notes)
+      
+        // for (var i = 0; i < characters.length; i++) {
+        //   if (chosen === characters[i].routeName) {
+        //     return res.json(characters[i]);
+        //   }
+        // }
+      
+        // return res.json(false);
+      
+
+    })
 }
 //     let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 
