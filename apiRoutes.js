5ce6007b47d9b1fe9229e80a63ab0707632d8f85
Laura Aupert <aupert.laura@gmail.com>
2021-02-02 02:44:59 -0500

    Created POST request for api/notes and note successfully appears on the left.

--- apiRoutes.js
+++ apiRoutes.js
@@ -5,10 +5,14 @@ module.exports = (app) => {
     app.get("/api/notes", function(req, res) {
         return res.json(notes)
     })
+
+    app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    notes.push(newNote)
+    console.log(newNote);
+    res.json(newNote);
+    })
 }
-
-
-
 //     let noteList = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 
 //     app.get("/api/notes", (req, res) => {
