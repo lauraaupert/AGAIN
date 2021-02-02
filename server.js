8b4801e2de4ab79a1bc2237e0747e250de9232a6
Laura Aupert <aupert.laura@gmail.com>
2021-02-02 12:48:25 -0500

    Cleaned up server.js some more

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
 const PORT = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.static('./'));
 
-//for json parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
